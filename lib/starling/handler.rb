b5ceac347c06307193ad3c8aa55f61c3b718f4d5
Robey Pointer <robey@twitter.com>
2008-03-26 18:05:07 -0700

    the "crazy" condition we were trying to log, because we didn't think it should be happening, in fact was not happening. so no need to log about it.

--- lib/starling/handler.rb
+++ lib/starling/handler.rb
@@ -162,10 +162,6 @@ STAT queue_%s_expired_items %d\n".freeze
       while response = @queue_collection.take(key)
         expiry, data = response.unpack(DATA_PACK_FMT)
 
-        if expiry.nil? 
-          logger.info "[CRAZY!] [q: #{key}] [m:get got data with *NO* EXPIRY #{expiry.inspect}] #{data.inspect}"
-        end
-
         break if expiry == 0 || expiry >= now
 
         @expiry_stats[key] += 1
