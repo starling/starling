70465a271211fa72c7a1f9122e1d56863f104cd5
Ed Robinson <ed.robinson@reevoo.com>
2014-11-11 14:32:53 +0000

    Fixup spec

--- spec/starling_server_spec.rb
+++ spec/starling_server_spec.rb
@@ -17,9 +17,9 @@ def safely_fork(&block)
   # anti-race juice:
   blocking = true
   Signal.trap("USR1") { blocking = false }
-  
+
   pid = Process.fork(&block)
-  
+
   while blocking
     sleep 0.1
   end
@@ -30,7 +30,7 @@ end
 describe "StarlingServer" do
   before do
     @tmp_path = File.join(File.dirname(__FILE__), "tmp")
-    
+
     begin
       Dir::mkdir(@tmp_path)
     rescue Errno::EEXIST
@@ -54,7 +54,7 @@ describe "StarlingServer" do
     @client = MemCache.new('127.0.0.1:22133')
 
   end
-  
+
   it "should test if temp_path exists and is writeable" do
     File.exist?(@tmp_path).should be_true
     File.directory?(@tmp_path).should be_true
@@ -67,16 +67,16 @@ describe "StarlingServer" do
     @client.set('test_set_and_get_one_entry', v)
     @client.get('test_set_and_get_one_entry').should eql(v)
   end
-  
+
   it "should respond to delete" do
-    @client.delete("my_queue").should eql("NOT_FOUND\r\n")   
+    @client.delete("my_queue").should eql("NOT_FOUND\r\n")
     starling_client = Starling.new('127.0.0.1:22133')
     starling_client.set('my_queue', 50)
     starling_client.available_queues.size.should eql(1)
     starling_client.delete("my_queue")
     starling_client.available_queues.size.should eql(0)
   end
-  
+
   it "should expire entries" do
     v = rand((2**32)-1)
     @client.get('test_set_with_expiry').should be_nil
@@ -97,7 +97,7 @@ describe "StarlingServer" do
     stats.has_key?('queue_test_age_age').should be_true
     (stats['queue_test_age_age'] >= 1000).should be_true
   end
-  
+
   it "should rotate log" do
     log_rotation_path = File.join(@tmp_path, 'test_log_rotation')
 
@@ -109,7 +109,7 @@ describe "StarlingServer" do
     v = 'x' * 8192
 
     @client.set('test_log_rotation', v)
-    File.size(log_rotation_path).should eql(8207)
+    File.size(log_rotation_path).should eql(8213)
     @client.get('test_log_rotation')
 
     @client.get('test_log_rotation').should be_nil
@@ -126,9 +126,9 @@ describe "StarlingServer" do
     stats = @client.stats
     stats.kind_of? Hash
     stats.has_key?('127.0.0.1:22133').should be_true
-    
+
     server_stats = stats['127.0.0.1:22133']
-    
+
     basic_stats = %w( bytes pid time limit_maxbytes cmd_get version
                       bytes_written cmd_set get_misses total_connections
                       curr_connections curr_items uptime get_hits total_items
@@ -152,21 +152,21 @@ describe "StarlingServer" do
     @client.reset
     @client.get('test_that_disconnecting_and_reconnecting_works').should eql(v)
   end
-  
+
   it "should use epoll on linux" do
     # this may take a few seconds.
     # the point is to make sure that we're using epoll on Linux, so we can
     # handle more than 1024 connections.
-    
+
     unless IO::popen("uname").read.chomp == "Linux"
-      pending "skipping epoll test: not on Linux"
+      skip "skipping epoll test: not on Linux"
     end
 
     fd_limit = IO::popen("bash -c 'ulimit -n'").read.chomp.to_i
     unless fd_limit > 1024
-      pending "skipping epoll test: 'ulimit -n' = #{fd_limit}, need > 1024"
+      skip "skipping epoll test: 'ulimit -n' = #{fd_limit}, need > 1024"
     end
-    
+
     v = rand(2**32 - 1)
     @client.set('test_epoll', v)
 
@@ -197,7 +197,7 @@ describe "StarlingServer" do
       Process.kill("TERM", pid2)
     end
   end
-  
+
   it "should raise error if queue collection is an invalid path" do
     invalid_path = nil
     while invalid_path.nil? || File.exist?(invalid_path)
@@ -208,7 +208,7 @@ describe "StarlingServer" do
       StarlingServer::QueueCollection.new(invalid_path)
     }.should raise_error(StarlingServer::InaccessibleQueuePath)
   end
-  
+
   after do
     Process.kill("INT", @server_pid)
     Process.wait(@server_pid)
